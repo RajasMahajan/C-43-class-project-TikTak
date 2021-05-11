dba1696c7e577a314ecd94b4397d001e3d0de386
RajasMahajan <73475718+RajasMahajan@users.noreply.github.com>
2021-05-11 20:55:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,29 +14,29 @@ function draw() {
   push();
   rotate(scAngle);
   stroke("green");
-  strokeWeight(7);
+  strokeWeight(5);
   line(0,0,100,0);
   pop();
   
-  scAngle = map(mn,60,0,0,360);
+  scAngle = map(mn,0,60,0,360);
   translate(0,1.5);
   
   push();
   rotate(scAngle);
   stroke("blue");
   strokeWeight(7);
-  line(0,0,100,0);
+  line(0,0,95,0);
   pop();
   
   
-  scAngle = map(hr,60,0,0,360);
+  scAngle = map(hr,0,12,0,360);
   translate(0,1.5);
   
   push();
   rotate(scAngle);
   stroke("red");
   strokeWeight(7);
-  line(0,0,100,0);
+  line(0,0,80,0);
   pop();
   
   // drawSprites();
